8cec069404a7e8dee66d263f1586c8019e5c18a1
Nacera-Z <127423997+Nacera-Z@users.noreply.github.com>
2023-03-09 11:01:48 -0500

    Create poeme.py

---
+++ poeme.py
@@ -0,0 +1,5 @@
+print("Montre à tes amis ton cœur et ta bonne foi, 
+Montre ton front à tous tes adversaires. 
+Fidèle à ta nature et conforme à ta loi : 
+Laisse dire les sots, écoute les sincères, 
+Consulte les sensés et marche devant toi.")
